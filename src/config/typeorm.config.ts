9b0138742f51f550c7dec30d1471c2ce4ed3791b
Nikolas Zacharioudakis <zacharioudakis.nikolas@gmail.com>
2024-05-15 18:09:32 +0300

    Remove unused ConfigService from TypeOrmConfigService

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 import { datasourceOptions } from './typeorm/data-source';
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
-  constructor(private configService: ConfigService) {}
-
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return datasourceOptions;
   }
