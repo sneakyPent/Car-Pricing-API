284befb2438ee797cce64c8975688ac1555d5ecf
Nikolas Zacharioudakis <zacharioudakis.nikolas@gmail.com>
2024-05-15 18:00:40 +0300

    Make db name to be variable based on env file

--- test/setup.ts
+++ test/setup.ts
@@ -1,8 +1,14 @@
 import { rm } from 'fs/promises';
 import { join } from 'path';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+import * as process from 'node:process';
+
+const envFile = path.resolve(`.env.${process.env.NODE_ENV}`);
+dotenv.config({ path: envFile });
 
 global.beforeEach(async () => {
   try {
-    await rm(join(__dirname, '..', 'test.sqlite'));
+    await rm(join(__dirname, '..', process.env.DATABASE_NAME));
   } catch (e) {}
 });
